97930273c099d58f083479eddd549e1d189ecff4
juanp <juanp@DESKTOP>
2021-03-08 02:43:45 -0600

    Entrega final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -174,6 +174,7 @@ namespace Laboratorio_No2_repaso_Joel_Gramajo
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Control de alquileres";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
